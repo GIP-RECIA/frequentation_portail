bf07cd191bec39ab880e290e0354fb9998fca6fb
lvillanne-recia <loic.villanne@recia.fr>
2022-02-22 18:36:00 +0100

    feat: modification du fonctionnement des droits

--- app/DroitsUtilisateur.php
+++ app/DroitsUtilisateur.php
@@ -17,6 +17,12 @@ class DroitsUtilisateur {
     private const GRP_ADMIN_LYCEE = 'esco:admin:Indicateurs:central';
     /** @var string Pour être admin des collèges d'un département, il faut faire partie de ce groupe */
     private const GRP_ADMIN_COLLEGE = 'clg%DEP%:admin:Indicateurs:central';
+    /** @var string Pour être admin cfa agricole, il faut faire partie de ce groupe */
+    private const GRP_ADMIN_CFA_AGRI = 'agri:admin:Indicateurs:central';
+    /** @var string Pour être admin cfa, il faut faire partie de ce groupe */
+    private const GRP_ADMIN_CFA = 'cfa:admin:Indicateurs:central';
+    /** @var string Pour être admin ef2s, il faut faire partie de ce groupe */
+    private const GRP_ADMIN_EF2S = 'ef2s:admin:Indicateurs:central';
 
     /** @var string Le champ de l'uai */
     private const UAI = 'escouai';
@@ -24,7 +30,6 @@ class DroitsUtilisateur {
     /** @var array */
     private $tabSiren = [];
 
-    // TODO: voir si les autres variables sont utiles a part le tabSiren
     /**
      * Constructeur
      **/
@@ -76,14 +81,12 @@ class DroitsUtilisateur {
         $sr = ldap_search($this->ldap, $dn, "(uid={$uid})", self::ATTR_A_RECUP_USER);
         $userInfos = ldap_get_entries($this->ldap, $sr);
         $groupsAdminCollege = [];
-        $isAdminLycee = false;
-        $isAdminCollege = [];
+        $filters = [];
 
         // Construction du tableau de la liste des groupes admin de collège a tester
         //  et affectation à false du fait d'être admin par un de ces groupes
         foreach ($config->get('departments') as $department) {
             $groupsAdminCollege[$department] = str_replace("%DEP%", $department, self::GRP_ADMIN_COLLEGE);
-            $isAdminCollege[$department] = false;
         }
 
         if ($userInfos === false) {
@@ -97,103 +100,75 @@ class DroitsUtilisateur {
         for ($i = 0; $i < $nbGroupes; $i++) {
             $groupe = $userInfos[0]['ismemberof'][$i];
 
-            // Enregistre si l'utilisateur est admin pour les lycées
-            if ($groupe === self::GRP_ADMIN_LYCEE) {
-                $isAdminLycee = true;
+            // Enregistre si l'utilisateur est admin dans un des cas
+            switch ($groupe) {
+                case self::GRP_ADMIN_LYCEE:
+                    $filters[] = "(entstructuretypestruct=LYCEE *)";
+                    $filters[] = "(entstructuretypestruct=ETABLISSEMENT REGIONAL D'ENSEIGNT ADAPTE)";
+                    break;
+                case self::GRP_ADMIN_CFA_AGRI:
+                    $filters[] = "(entstructuretypestruct=CFA AGRICOLE)";
+                    break;
+                case self::GRP_ADMIN_CFA:
+                    $filters[] = "(entstructuretypestruct=CFA)";
+                    break;
+                case self::GRP_ADMIN_EF2S:
+                    $filters[] = "(entstructuretypestruct=EF2S)";
+                    $filters[] = "(entstructuretypestruct=UNIV)";
+                    break;
             }
 
             // Enregistre si l'utilisateur est admin pour les collèges d'un département
             foreach ($groupsAdminCollege as $department => $grpAdminCollegeDep) {
                 if ($groupe === $grpAdminCollegeDep) {
-                    $isAdminCollege[$department] = true;
+                    $filters[] = "(&(entstructuretypestruct=COLLEGE)(entstructureuai=0{$department}*))";
                 }
             }
         }
 
-        // Construction de la liste des uai e l'utilisateur
+        // Construction de la liste des uai de l'utilisateur
         // * Si utilisateur lambda : ses uai
-        // * Si admin de groupe (lycée, collège d'un département), tous les établissement du groupe
+        // * Si admin de groupe (lycée, collège d'un département, ...), tous les établissement du groupe
         // * sinon erreur
         $dn_base = explode(",", $dn, 2);
         $dn = "ou=structures,{$dn_base[1]}";
-        $this->addSirensIfAdmin($isAdminLycee,
-            "(entstructuretypestruct=LYCEE *)", $dn, "Lycées non trouvés dans le ldap");
-
-        foreach ($isAdminCollege as $department => $isAdmin) {
-            $this->addSirensIfAdmin($isAdmin,
-                "(entstructuretypestruct=COLLEGE)(entstructureuai=0{$department}*)", $dn,
-                "Collèges pour le département {$department} non trouvés dans le ldap");
-        }
 
         // Si l'utilisateur est liè a un ou des établissement, on les ajoutes
         if (isset($userInfos[0][self::UAI])) {
             $countUai = $userInfos[0][self::UAI]['count'];
-            $uaiForUser = [];
 
-            if ($countUai > 0) {
-                for ($i = 0; $i < $countUai; $i++) {
-                    $uaiForUser[] =$userInfos[0][self::UAI][$i];
-                }
-            } else {
+            if ($countUai === 0) {
                 throw new Exception("Impossible de récupérer les uai liés à l'utilisateur");
             }
 
-            foreach ($uaiForUser as $uaiEtab) {
-                $filtre = "(entstructureuai={$uaiEtab})";
-                $sr = ldap_search($this->ldap, $dn, $filtre, self::ATTR_A_RECUP_STRUCT);
-                $etab = ldap_get_entries($this->ldap, $sr);
-
-                if ($etab === false) {
-                    throw new Exception("Impossible de trouver l'établissement {$uaiEtab} dans le ldap");
-                }
-
-                if ($etab[0]['entstructuresiren']['count'] === 0) {
-                    throw new Exception("Impossible de récupérer le SIREN pour l'établissement {$uaiEtab}");
-                }
-
-                $this->addSiren($etab[0]['entstructuresiren'][0]);
+            for ($i = 0; $i < $countUai; $i++) {
+                $filters[] = "(entstructureuai=".$userInfos[0][self::UAI][$i].")";
             }
         }
 
-        if (count($this->tabSiren) === 0) {
-            throw new Exception("L'utilisateur ne peut voir aucun établissement");
-        }
-    }
-
-    /**
-     * Liste les siren en fonction d'un filtre si l'on est admin
-     *
-     * @param bool   $isAdmin      Permet de savoir si l'on est admin pour ce filtre
-     * @param string $subFilter    Le filtre de recherche
-     * @param string $dn
-     * @param string $errorMessage Le message en cas d'erreur
-     */
-    private function addSirensIfAdmin(bool $isAdmin, string $subFilter, string $dn, string $errorMessage): void {
-        if ($isAdmin) {
-            $filter = "(&(objectclass=ENTEtablissement){$subFilter})";
+        // On joue la requête avec le filtre et on ajoute tous les siren
+        if (count($filters) > 0) {
+            $filter = "(&(objectclass=ENTEtablissement)(|".implode($filters)."))";
             $sr = ldap_search($this->ldap, $dn, $filter, self::ATTR_A_RECUP_STRUCT);
             $etabs = ldap_get_entries($this->ldap, $sr);
 
             if ($etabs === false) {
-                throw new Exception($errorMessage);
+                throw new Exception("Impossible de trouver le ou les établissements dans le ldap avec le filtre ${$filter}");
             }
 
             foreach ($etabs as $key => $etab) {
                 if ($key !== "count" && $etab['entstructuresiren']['count'] > 0) {
-                    $this->addSiren($etab['entstructuresiren'][0]);
+                    $siren = $etab['entstructuresiren'][0];
+    
+                    if (!in_array($siren, $this->tabSiren, true)) {
+                        $this->tabSiren[] = $siren;
+                    }
                 }
             }
         }
-    }
 
-    /**
-     * Ajoute un siren a la liste des siren
-     * 
-     * @param string $siren Le siren a ajouter
-     */
-    private function addSiren(string $siren): void {
-        if (!in_array($siren, $this->tabSiren, true)) {
-            $this->tabSiren[] = $siren;
+        if (count($this->tabSiren) === 0) {
+            throw new Exception("L'utilisateur {$uid} ne peut voir aucun établissement : {$filter}");
         }
     }
 }
